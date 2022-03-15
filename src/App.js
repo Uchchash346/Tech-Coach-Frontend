f6bdcb4a74625be1ce834183ee93d690d2b37ba0
Uchchash346 <mru416@gmail.com>
2022-03-15 06:43:13 +0600

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import NoticeBar from './components/NoticeBar/NoticeBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<NoticeBar />} />
+      <Route path="/" element={<NavBar />} />
+    </Routes>
   );
 }
 
---
+++ src/components/NoticeBar/NoticeBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NoticeBar.css';
+
+const NoticeBar = () => {
+    return (
+        <>
+            <div className="notification-top-bar">
+                <p>Welcome to Tech Coach. Coach yourselves and get the desired job.<small><a href="https://www.w3schools.com/default.asp">Click Here</a></small></p>
+            </div>
+        </>
+    );
+};
+
+export default NoticeBar;
